4e998d69a9b9465b6b59263db3cd85979d3313ae
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-25 17:36:22 -0600

    Archivos iniciales

---
+++ src/main/java/com/cqcias/productoapp/ProductoappApplication.java
@@ -0,0 +1,13 @@
+package com.cqcias.productoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cqcias/productoapp/ProductoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqcias.productoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
