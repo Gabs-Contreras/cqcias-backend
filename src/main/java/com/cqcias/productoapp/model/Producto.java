fd8aadecd0cfcbf88194ef0b3ac402fe18df64b7
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-25 18:00:44 -0600

    Repository

--- src/main/java/com/cqcias/productoapp/model/Producto.java
+++ src/main/java/com/cqcias/productoapp/model/Producto.java
@@ -4,10 +4,72 @@
  */
 package com.cqcias.productoapp.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /**
  *
  * @author Gaby_Contreras
  */
 public class Producto {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String producto;
+    private long codigobarras;
+    private String marca;
+    private double costo;
+    private String estatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public long getCodigobarras() {
+        return codigobarras;
+    }
+
+    public void setCodigobarras(long codigobarras) {
+        this.codigobarras = codigobarras;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+   
     
 }
---
+++ src/main/java/com/cqcias/productoapp/repository/ProductoRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.cqcias.productoapp.repository;
+
+import com.cqcias.productoapp.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Gaby_Contreras
+ */
+public interface ProductoRepository extends JpaRepository <Producto, Long> {
+    
+}
